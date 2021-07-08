de153a610f280983b42545647378f55699515ea1
TanyaS08 <tanya.strydom@icloud.com>
2021-07-08 09:13:40 +0200

    stuff...

--- dashboard/DataPrep.R
+++ dashboard/DataPrep.R
@@ -20,6 +20,19 @@ library(tidytext)
 
 survey <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1V4AdCLm6UgTqO7UKjBFwAogBvPi1zWEjBZ37A2f4dcI/edit?usp=sharing')
 
+question_4 = 
+  survey %>%
+  select(-`Start time`, -`Completion time`, -Email, -Name, -Colonne1, -Colonne2,
+         -`What advice would you give your past-self or a young researcher from your country?`,
+         -`Describe your dream of the future as a tropical scientist in a tweet (up to 280 characters).`) %>%
+  unnest_tokens(word,
+                `Tell us a short story about your background in science! Do you want to share an image or a video that represents your story as a tropical/subtropical ecologist? Go ahead!`)
 
+question_4 %>%
+  filter(Language == "EN") %>%
+  anti_join(stop_words, 
+            by=c("word"="word")) %>%
+  left_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE)
 
 # End of script ----
