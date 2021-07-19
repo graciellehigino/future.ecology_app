7f491b80e4606a8aa5651c2ba2c7b6f6e97c5f20
TanyaS08 <tanya.strydom@icloud.com>
2021-07-19 09:47:55 +0200

    wordcloud for all

--- dashboard/DataPrep.R
+++ dashboard/DataPrep.R
@@ -22,9 +22,14 @@ library(reshape2)
 sent_pal = c('red', 'grey50', 'green')
 
 ### 1) Dataframes ----
-### >> a) Surveys ----
 
 survey <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1V4AdCLm6UgTqO7UKjBFwAogBvPi1zWEjBZ37A2f4dcI/edit?usp=sharing')
+
+### >> a) Question 4 ----
+
+# Tell us a short story about your background in science! Do you want to 
+# share an image or a video that represents your story as a tropical/subtropical 
+# ecologist? Go ahead!
 
 question_4 = 
   survey %>%
@@ -33,7 +38,7 @@ question_4 =
          -`Describe your dream of the future as a tropical scientist in a tweet (up to 280 characters).`) %>%
   unnest_tokens(word,
                 `Tell us a short story about your background in science! Do you want to share an image or a video that represents your story as a tropical/subtropical ecologist? Go ahead!`) %>%
-  filter(Language == "EN") %>%
+  filter(`Translated to` == "EN") %>%
   anti_join(stop_words, 
             by=c("word"="word")) %>%
   left_join(get_sentiments("bing")) %>%
@@ -50,6 +55,64 @@ question_4 =
   comparison.cloud(colors = sent_pal,
                    max.words = 100, scale=c(3.5,0.50))
 
+### >> b) Question 5 ----
+
+# What advice would you give your past-self or a young 
+# researcher from your country?
+
+question_5 = 
+  survey %>%
+  select(-`Start time`, -`Completion time`, -Email, -Name, -Colonne1, -Colonne2,
+         -`Tell us a short story about your background in science! Do you want to share an image or a video that represents your story as a tropical/subtropical ecologist? Go ahead!`,
+         -`Describe your dream of the future as a tropical scientist in a tweet (up to 280 characters).`) %>%
+  unnest_tokens(word,
+                `What advice would you give your past-self or a young researcher from your country?`) %>%
+  filter(`Translated to` == "EN") %>%
+  anti_join(stop_words, 
+            by=c("word"="word")) %>%
+  left_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  rename(`freq` = `n`) %>%
+  mutate(colour = as.character(case_when(sentiment == "negative" ~ "red",
+                                         sentiment == "positive" ~ "#E00008",
+                                         is.na(sentiment) ~ "white"))) %>%
+  mutate(sentiment = ifelse(is.na(sentiment),
+                            'neutral', sentiment)) %>% #rename 'non-sentiment'
+  filter(str_detect(word,
+                    '[:alpha:]')) %>% #remove numbers
+  acast(word ~ sentiment, value.var = "freq", fill = 0) %>% #reshape
+  comparison.cloud(colors = sent_pal,
+                   max.words = 100, scale=c(3.5,0.50))
+
+
+### >> b) Question 6 ----
+
+# Describe your dream of the future as a tropical scientist 
+# in a tweet (up to 280 characters).
+
+question_6 = 
+  survey %>%
+  select(-`Start time`, -`Completion time`, -Email, -Name, -Colonne1, -Colonne2,
+         -`Tell us a short story about your background in science! Do you want to share an image or a video that represents your story as a tropical/subtropical ecologist? Go ahead!`,
+         -`What advice would you give your past-self or a young researcher from your country?`) %>%
+  unnest_tokens(word,
+                `Describe your dream of the future as a tropical scientist in a tweet (up to 280 characters).`) %>%
+  filter(`Translated to` == "EN") %>%
+  anti_join(stop_words, 
+            by=c("word"="word")) %>%
+  left_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  rename(`freq` = `n`) %>%
+  mutate(colour = as.character(case_when(sentiment == "negative" ~ "red",
+                                         sentiment == "positive" ~ "#E00008",
+                                         is.na(sentiment) ~ "white"))) %>%
+  mutate(sentiment = ifelse(is.na(sentiment),
+                            'neutral', sentiment)) %>% #rename 'non-sentiment'
+  filter(str_detect(word,
+                    '[:alpha:]')) %>% #remove numbers
+  acast(word ~ sentiment, value.var = "freq", fill = 0) %>% #reshape
+  comparison.cloud(colors = sent_pal,
+                   max.words = 100, scale=c(3.5,0.50))
 
 
 # End of script ----
